6d4421e7d7ab6e8a087fcd3d1076efbadc6baa85
davepodgorski <davepodgorski@gmail.com>
2019-01-07 14:00:47 -0500

    lightswitch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+const text = document.getElementsByClassName('status');
+const switch = document.getElementById('button');
+const light = document.getElementsByClassName('light');
+
+switch.addEventListener('click', function() {
+   statusText.innertext =("Who turned out the lights?");
+});
 
 
+
+});

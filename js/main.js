dd4c7e6f2cbe9f2c9a070ba42a81db6dd1cc0220
davepodgorski <davepodgorski@gmail.com>
2019-01-07 18:07:59 -0500

    done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-const text = document.getElementsByClassName('status');
-const switch = document.getElementById('button');
-const light = document.getElementsByClassName('light');
+  var h1 = document.getElementsByTagName('h1')[0];
+  var button = document.getElementsByClassName("switch")[0];
+  var body = document.getElementsByTagName('body')[0];
+  var on = document.getElementsByClassName("on")[0];
+  var off = document.getElementsByClassName("off")[0];
 
-switch.addEventListener('click', function() {
-   statusText.innertext =("Who turned out the lights?");
-});
+      button.addEventListener('click', function() {
+        if(body.classList.contains("light")) {
+        body.classList.remove("light");
+        body.classList.add("dark");
+        button.classList.remove("on");
+        button.classList.add("off");
+        h1.innerText = "Hey, who turned off the lights?";
 
-
-
+      } else {
+        body.classList.remove("dark");
+        body.classList.add("light");
+        button.classList.remove("off");
+        button.classList.add("on");
+        h1.innerText = "It's so bright in here!";
+      }
+    });
 });
